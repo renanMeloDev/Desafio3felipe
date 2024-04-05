e89bd494a4b0ce9843df80b2d85df1830c6b9963
renanMeloDev <dev.renanmelo@gmail.com>
2024-04-05 11:52:56 -0300

    versão final

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-class mestreDosMagos{
-    nome=`Mago Alado`;
-    age = 85;
-    type= `Mago` ;
+class magos{
+    constructor(nome, age, type,ataque){
+        this.nome=nome
+        this.age=age
+        this.type=type 
+          
+    }
+    atack (){
+        console.log (` o `+ this.type)
+    }
 }
-constructor (nome, age, type);{
-    this.nome=nome
-    this.age=age
-    this.type=type
-}
-atack();{
- console.log ("o" + this.type + ("atacou usando a magia dos lagos azuis"))
-}
-atack()
+         
+    let mestreDosMagos = new magos (`Mestre dos Magos`, 85, `Mago`)
+
+ mestreDosMagos.atack()
+ 
+ let ataque = mestreDosMagos.type
+    if (ataque === `Mago`)
+    console.log (`atacou usando a Magia dos Lagos Azuis`)
+        else if (ataque === `guerreiro`)
+        console.log(` `)
+        else if (ataque === `monge`)
+        console.log(` `)
+        else if (ataque === `ninja`)
+        console.log(` `)
