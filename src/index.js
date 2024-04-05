11120b147f7b0e82347d5f0d9db1e7776e21e9d3
renanMeloDev <dev.renanmelo@gmail.com>
2024-04-05 09:46:23 -0300

    segundo commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+class mestreDosMagos{
+    nome=`Mago Alado`;
+    age = 85;
+    type= `Mago` ;
+}
+constructor (nome, age, type);{
+    this.nome=nome
+    this.age=age
+    this.type=type
+}
+atack();{
+ console.log ("o" + this.type + ("atacou usando a magia dos lagos azuis"))
+}
+atack()
